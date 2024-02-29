42ad0900122c9e74a8441ebf1f8068eda62378eb
aisha <aisha_mo@hotmail.com>
2024-02-29 16:35:09 +0000

    created button, helpRequestList and homepage componenets

---
+++ src/componenets/HelpRequestList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelpRequestList = () => {
+    
+    return (
+        <div>
+            <h1>HelpRequest</h1>
+        </div>
+    )
+}
+
+export default HelpRequestList
---
+++ src/componenets/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+import HelpRequestList from './HelpRequestList'
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <HelpRequestList />
+            <Button />
+        </div>
+    )
+}
+
+export default Homepage
