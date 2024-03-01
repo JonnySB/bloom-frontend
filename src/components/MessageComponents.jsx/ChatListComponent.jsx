8c9b1d1a4f976980341e411f45b5ba7ec9e3b2ea
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-01 16:48:26 +0000

    front end messages map created

--- src/components/MessageComponents.jsx/ChatListComponent.jsx
+++ src/components/MessageComponents.jsx/ChatListComponent.jsx
@@ -2,20 +2,37 @@ import React, { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
+import { getAllMessagesByUserId } from "../../services/messages";
 
 function ChatListCompoenent() {
-  return (
-    <Container > 
-        <Card style={{ width: '18rem' }}>
-        <Card.Header>Messages</Card.Header>
-        <ListGroup variant="flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
-        </Card>
-    </Container>
-  );
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let messagesData = await getAllMessagesByUserId(2);
+                setMessages(messagesData);
+            } catch (err) {
+                console.error('Error fetching messages:', err);
+            }
+        };
+        fetchData();
+    }, []);
+
+    console.log(messages)
+    return (
+        <Container>
+          <Card style={{ width: '20rem' }}>
+            <Card.Header>Messages</Card.Header>
+            <ListGroup variant="flush">
+              {messages.map((message, index) => (
+                <ListGroup.Item key={index}> From user {message.sender_id}</ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Card>
+        </Container>
+      );
+      
 }
 
 export default ChatListCompoenent;
--- src/services/messages.js
+++ src/services/messages.js
@@ -2,27 +2,24 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 
-export const getAllMessagesByUserId = async (user_id) => {
-    const payload = {
-        user_id: user_id,
-    };
-
+export const getAllMessagesByUserId = async (userId) => {
     const requestOptions = {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
+            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwOTMxMDQ0NSwianRpIjoiZTJlNjllYWUtYmMxOC00NzZlLWFkMmUtZTE0ODk2NWUwZDEyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNzA5MzEwNDQ1LCJjc3JmIjoiMjA4MzkyZGQtNzQxZS00MmFlLThmODMtNGI1NGE4NjQyZGJmIiwiZXhwIjoxNzA5Mzk2ODQ1fQ.8CB3gQsS-UPEjnQH4I9lkkLuVDn-OnV5QKVXm-HTghY`,
         },
-        body: JSON.stringify(payload),
     };
     
-    const response = await fetch(`${BACKEND_URL}/messages/`, requestOptions)
+    const response = await fetch(`${BACKEND_URL}/messages/user/${userId}`, requestOptions);
 
     if (response.status === 200) {
         const data = await response.json();
-        console.log("Messages loaded ")
-        return data
-      } else {
-        const errorResponse = await response.json(); 
-        throw new Error(errorResponse);
+        console.log("Messages loaded ");
+        return data;
+    } else {
+        const errorResponse = await response.json();
+        console.error('Full error response:', errorResponse);
+        throw new Error(`Error fetching messages: ${errorResponse}`);
     }
 }
