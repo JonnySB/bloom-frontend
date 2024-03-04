6b5e85fd78d6d6b02615ba45f5156e0ce3e235d2
aisha <aisha_mo@hotmail.com>
2024-03-04 13:26:05 +0000

    completed tests for get all help requests and get one help request services

--- tests/services/HelpRequest.test.js
+++ tests/services/HelpRequest.test.js
@@ -1,55 +1,46 @@
 import { describe, expect, vi } from "vitest";
 import createFetchMock from "vitest-fetch-mock";
-
-import { getAllHelpRequests,getHelpRequestById } from "../../src/services/HelpRequests";
+import { getAllHelpRequestsWithUserDetails, getOneHelpRequestById} from "../../src/services/HelpRequests";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const mockHelpRequestId = "22";
 
 createFetchMock(vi).enableMocks();
 
-// vi.mock("../../src/services/HelpRequests", () => {
-//     const getUserByIdMock = vi.fn();
-//         // return Promise.resolve({ id, name: "Test User" });
-//     return {getUserById : getUserByIdMock}
-
-// });
-
 describe("Help Request Service", () => {
-    // describe("Get all help requests", () => {
-    //     test("Gets help requests with status code 200", async () => {
-    //         // mock a fetch response
-    //         fetch.mockResponseOnce(JSON.stringify({ helpRequests: [] }), 
-    //         { status: 200 });
+    describe("Get all help requests", () =>{
+        test("Gets help requests with status code 200", async () => {
+            // mock a fetch response
+            fetch.mockResponseOnce(JSON.stringify({ helpRequests: [] }), 
+            { status: 200 });
+            await getAllHelpRequestsWithUserDetails();
 
-    //         await getAllHelpRequests();
+            const fetchArguments = fetch.mock.lastCall;
+            const url = fetchArguments[0];
+            const options = fetchArguments[1];
 
-    //         const fetchArguments = fetch.mock.lastCall;
-    //         const url = fetchArguments[0];
-    //         const options = fetchArguments[1];
+            expect(url).toEqual(`${BACKEND_URL}/help_requests2`);
+            expect(options.method).toEqual("GET");
+        });
 
-    //         expect(url).toEqual(`${BACKEND_URL}/help_requests`);
-    //         expect(options.method).toEqual("GET");
-    //     });
+        test("Rejects GET request with an error if the status is NOT 200", async () => {
+            fetch.mockResponseOnce(
+                JSON.stringify({ message: "Something went wrong" }),
+                { status: 400 }
+            );
 
-    //     test("Rejects GET request with an error if the status is NOT 200", async () => {
-    //         fetch.mockResponseOnce(
-    //             JSON.stringify({ message: "Something went wrong" }),
-    //             { status: 400 }
-    //         );
-    //         try {
-    //             await getAllHelpRequests();
-    //         } catch(error) {
-    //             expect(error.message).toEqual("Failed to fetch all help requests")
-    //         }
-    //     });
-    // });
+            try {
+                await getAllHelpRequestsWithUserDetails();
+            } catch(error) {
+                expect(error.message).toEqual("Failed to fetch all help requests with user details")
+            }
+        });
+    })
 
     describe("Get one help request", () => {
-        test("Successful get help request by id with status code 200", async () => {
+        test("Successful get help request by id with status code 200", async () =>  {
             fetch.mockResponseOnce(JSON.stringify({}),{ status: 200 });
-            
-            await getHelpRequestById(mockHelpRequestId);
+            await getOneHelpRequestById(mockHelpRequestId);
 
             const fetchArguments = fetch.mock.lastCall;
             const url = fetchArguments[0];
@@ -58,40 +49,15 @@ describe("Help Request Service", () => {
             expect(url).toEqual(`${BACKEND_URL}/help_requests/22`)
             expect(options.method).toEqual("GET");
         });
-        
+
         test("Unsuccessful get help request by id when status NOT 200", async () => {
             fetch.mockResponseOnce(JSON.stringify({message: "Something went wrong"}),{ status: 400 });
+
             try{
-                await getHelpRequestById("");
+                await getOneHelpRequestById("");
             } catch(error){
-                expect(error.message).toEqual("Failed to fetch help request");
+                expect(error.message).toEqual("Failed to fetch help request with id "); // make sure there is a spave between id and "
             }
-        })
+        });
     })
-
-    // describe("Get all help requests with users", () => {
-    //     test("Gets help requests with status code 200", async () => {
-    //         fetch.mockResponseOnce(JSON.stringify([{ id: 1, user_id: "user1" }]), { status: 200 });
-
-    //         const helpRequestsWithUsers = await getAllHelpRequestsWithUsers();
-
-    //         expect(helpRequestsWithUsers).toEqual([{ id: 1, user_id: "user1", user: { id: "user1", name: "Test User" } }]);
-    //         const fetchArguments = fetch.mock.lastCall;
-    //         const url = fetchArguments[0];
-    //         const options = fetchArguments[1];
-
-    //         expect(url).toEqual(`${BACKEND_URL}/help_requests`);
-    //         expect(options.method).toEqual("GET");
-    //     });
-
-    //     test("Rejects GET request with an error if the status is NOT 200", async () => {
-    //         fetch.mockResponseOnce(JSON.stringify({ message: "Something went wrong" }), { status: 400 });
-
-    //         try {
-    //             await getAllHelpRequestsWithUsers();
-    //         } catch (error) {
-    //             expect(error.message).toEqual("Failed to fetch all help requests");
-    //         }
-    //     });
-    // });
 });
