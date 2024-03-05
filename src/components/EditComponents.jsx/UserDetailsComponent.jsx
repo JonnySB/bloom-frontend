a0bdefa1599f6133acce7f4d2736683a29db679d
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-05 18:36:25 +0000

    edit page finally working

--- src/components/EditComponents.jsx/UserDetailsComponent.jsx
+++ src/components/EditComponents.jsx/UserDetailsComponent.jsx
@@ -1,3 +1,4 @@
+
 import { Container, Nav, Navbar, Card, ListGroup, Modal, Button, Form }from 'react-bootstrap';
 import "./UserDetails.css"
 import { useState, useEffect } from 'react';
@@ -5,9 +6,8 @@ import { editUsersInformation } from '../../services/users';
 
 function UserNavbar({ userDetails }) {
     const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const [text, setText] = useState('');
+    const [formDetails, setFormDetails] = useState({});
+    const [newFormDetails, setNewFormDetails] = useState({});
     const [inputVisibility, setInputVisibility] = useState({
         firstName: false,
         lastName: false,
@@ -17,19 +17,51 @@ function UserNavbar({ userDetails }) {
         address: false,
     });
 
+    const handleShow = () => {
+        setFormDetails({
+            firstName: userDetails?.first_name || '',
+            lastName: userDetails?.last_name || '',
+            userName: userDetails?.username || '',
+            email: userDetails?.email || '',
+            avatar: userDetails?.avatar_url_string || '',
+            address: userDetails?.address || '',
+        });
+        setShow(true);
+    };
+ 
+  
     const toggleInputVisibility = field => {
         setInputVisibility(prev => ({ ...prev, [field]: !prev[field] }));
     };
 
     const handleChange = (e, field) => {
-        // Handle your input change logic here
-        // This is just a placeholder function to illustrate
-        setText(e.target.value);
+        setFormDetails(prevDetails => ({
+            ...prevDetails,
+            [field]: e.target.value,
+        }));
     };
-
-
-
-
+ 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        
+        // Prepare newFormDetails with either new data or fallback to original data
+        const updatedFormDetails = {
+            userId: userDetails.id,
+            firstName: formDetails.firstName || userDetails.first_name,
+            lastName: formDetails.lastName || userDetails.last_name,
+            userName: formDetails.userName || userDetails.username,
+            email: formDetails.email || userDetails.email,
+            address: formDetails.address || userDetails.address,
+        };
+    
+        try {
+            await editUsersInformation(updatedFormDetails);
+            console.log("form submited")
+            // Optionally, trigger a refresh of userDetails from the parent component or context
+        } catch (err) {
+            console.log('Edit not completed',err);
+        }
+    };
 
     return (
         <>
@@ -58,40 +90,40 @@ function UserNavbar({ userDetails }) {
             </div>
         </Container>
         <Modal show={show} onHide={() => setShow(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Edit your information</Modal.Title>
-                </Modal.Header>
-                <Form>
-                    <Modal.Body>
-                        {inputVisibility.firstName ? 
-                            <input type="text" value={text} onChange={e => handleChange(e, 'firstName')} /> : 
-                            <>{userDetails?.first_name}<Button variant="primary" onClick={() => toggleInputVisibility('firstName')}>🖊️</Button></>}
-                    </Modal.Body>
-                    <Modal.Body>
-                        {inputVisibility.lastName ? 
-                            <input type="text" value={text} onChange={e => handleChange(e, 'lastName')} /> : 
-                            <>{userDetails?.last_name}<Button variant="primary" onClick={() => toggleInputVisibility('lastName')}>🖊️</Button></>}
-                    </Modal.Body>
-                    <Modal.Body>
-                        {inputVisibility.userName ? 
-                            <input type="text" value={text} onChange={e => handleChange(e, 'userName')} /> : 
-                            <>{userDetails?.username}<Button variant="primary" onClick={() => toggleInputVisibility('userName')}>🖊️</Button></>}
-                    </Modal.Body>
-                    <Modal.Body>
-                        {inputVisibility.email ? 
-                            <input type="text" value={text} onChange={e => handleChange(e, 'email')} /> : 
-                            <>{userDetails?.email}<Button variant="primary" onClick={() => toggleInputVisibility('email')}>🖊️</Button></>}
-                    </Modal.Body>
-                    <Modal.Body>
-                        {inputVisibility.address ? 
-                            <input type="text" value={text} onChange={e => handleChange(e, 'address')} /> : 
-                            <>{userDetails?.address}<Button variant="primary" onClick={() => toggleInputVisibility('address')}>🖊️</Button></>}
-                    </Modal.Body>
-                </Form>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShow(false)}>Close</Button>
-                    <Button variant="primary" onClick={() => setShow(false)}>Save Changes</Button>
-                </Modal.Footer>
+            <Modal.Header closeButton>
+                <Modal.Title>Edit your information</Modal.Title>
+            </Modal.Header>
+            <Form id="userEditForm" onSubmit={handleFormSubmit}>
+                <Modal.Body>
+                    {inputVisibility.firstName ? 
+                        <input type="text" value={formDetails.firstName} onChange={e => handleChange(e, 'firstName')} /> : 
+                        <>{userDetails?.first_name}<Button variant="primary" onClick={() => toggleInputVisibility('firstName')}>🖊️</Button></>}
+                </Modal.Body>
+                <Modal.Body>
+                    {inputVisibility.lastName ? 
+                        <input type="text" value={formDetails.lastName} onChange={e => handleChange(e, 'lastName')} /> : 
+                        <>{userDetails?.last_name}<Button variant="primary" onClick={() => toggleInputVisibility('lastName')}>🖊️</Button></>}
+                </Modal.Body>
+                <Modal.Body>
+                    {inputVisibility.username ? 
+                        <input type="text" value={formDetails.username} onChange={e => handleChange(e, 'username')} /> : 
+                        <>{userDetails?.username}<Button variant="primary" onClick={() => toggleInputVisibility('username')}>🖊️</Button></>}
+                </Modal.Body>
+                <Modal.Body>
+                    {inputVisibility.email ? 
+                        <input type="text" value={formDetails.email} onChange={e => handleChange(e, 'email')} /> : 
+                        <>{userDetails?.email}<Button variant="primary" onClick={() => toggleInputVisibility('email')}>🖊️</Button></>}
+                </Modal.Body>
+                <Modal.Body>
+                    {inputVisibility.address ? 
+                        <input type="text" value={formDetails.address} onChange={e => handleChange(e, 'address')} /> : 
+                        <>{userDetails?.address}<Button variant="primary" onClick={() => toggleInputVisibility('address')}>🖊️</Button></>}
+                </Modal.Body> 
+            </Form>
+            <Modal.Footer>
+             <Button variant="secondary" onClick={() => setShow(false)}>Close</Button>
+             <Button variant="primary" type="submit" form="userEditForm">Save Changes</Button>
+            </Modal.Footer>
         </Modal>
     </>
     );
--- src/services/users.js
+++ src/services/users.js
@@ -21,32 +21,47 @@ export const getuserInformationById = async (userId) => {
 }
 }
 
-export const editUsersInformation = async (user_id, first_name, last_name, user_picture) => {
+export const editUsersInformation = async (form) => {
+    console.log("Preparing to send edit request with form data:", form);
+
     const requestOptions = {
-      method: "PUT",
-      headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwOTYzMTA0MiwianRpIjoiYTE2OGFiMjEtNjY1OS00ODYwLTlhYzItYWIwMTcyN2IxNmRlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNzA5NjMxMDQyLCJjc3JmIjoiZDg5MGUxZGUtNjE1NS00NDQzLTg2OWMtMzE1NGQyMWQ3ZWZkIiwiZXhwIjoxNzA5NzE3NDQyfQ.xM1DsphGgXDa-3L8G-1BZBhhtah1L933upizto-j3v8`,
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwOTYzMTA0MiwianRpIjoiYTE2OGFiMjEtNjY1OS00ODYwLTlhYzItYWIwMTcyN2IxNmRlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNzA5NjMxMDQyLCJjc3JmIjoiZDg5MGUxZGUtNjE1NS00NDQzLTg2OWMtMzE1NGQyMWQ3ZWZkIiwiZXhwIjoxNzA5NzE3NDQyfQ.xM1DsphGgXDa-3L8G-1BZBhhtah1L933upizto-j3v8`,
+        },
+        body: JSON.stringify({
+            first_name: form.firstName,
+            last_name: form.lastName,
+            username: form.userName,
+            email: form.email,
+            address: form.address,
+        }),
+    };
+    
+    console.log("Request options:", requestOptions);
 
-      },
+    const response = await fetch(`${BACKEND_URL}/edit_user_details/${form.userId}`, requestOptions);
 
-      body: JSON.stringify({
-        user_id: userId,
-        first_name: first_name,
-        last_name: last_name,
-        user_picture: user_picture,
-    }),
-  };
+    console.log("Response received", response);
 
-  const response = await fetch(`${BACKEND_URL}/edit/${user_id}`, requestOptions);
-
-  if (response.status === 200) {
-    const data = await response.json();
-    // console.log("user details loaded loaded ");
-    return data;
-  } else {
-    const errorResponse = await response.json();
-    console.error('Full error response:', errorResponse);
-    throw new Error(`Error fetching userdetails : ${errorResponse}`);
-}
-}
+    if (response.ok) {
+        try {
+            const data = await response.json();
+            console.log("Response data loaded successfully:", data);
+            return data;
+        } catch (error) {
+            console.error("Failed to parse response JSON:", error);
+            throw new Error("Failed to parse response JSON.");
+        }
+    } else {
+        console.error("Response status was not OK:", response.status);
+        try {
+            const errorResponse = await response.json();
+            console.error('Full error response:', errorResponse);
+            throw new Error(`Error from server: ${errorResponse.message || 'Unknown error'}`);
+        } catch (error) {
+            throw new Error("Failed to parse error response JSON.");
+        }
+    }
+};
