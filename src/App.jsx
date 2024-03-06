526b53ad3753320bd0e8ea0a114d4c463dda13b9
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-03-06 15:51:24 +0000

    removed index route

--- src/App.jsx
+++ src/App.jsx
@@ -40,10 +40,6 @@ const router = createBrowserRouter([
         element: <Login />,
     },
     {
-        path: "/add_plant",
-        element: <Index />
-    },
-    {
         path: "/signup",
         element: <Signup />,
     },
