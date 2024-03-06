1d81e8677e4637da6a8a24bd5d43be036e546e40
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-03-06 15:50:00 +0000

    updated empty route for index

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Signup } from "./pages/Signup/SignupPage";
 import { MyPlants } from "./pages/MyPlants/MyPlants"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./pages/MyPlants/MyPlants.css"
-import { Profile } from "./pages/Profile/ProfilePage";import { Index } from "./pages/Index/Index";
+import { Profile } from "./pages/Profile/ProfilePage"; import { Index } from "./pages/Index/Index";
 import ReceivedOffersPage from "./pages/ManageHelpRequestPages/ReceivedOffersPage/ReceivedOffersPage";
 import './App.css'
 import Homepage from './pages/Home/Homepage';
@@ -15,14 +15,14 @@ import CreateRequestPage from './pages/CreateRequestPage/CreateRequestPage';
 import MyOffersPage from "./pages/ManageHelpRequestPages/MyOffersPage/MyOffersPage";
 
 const router = createBrowserRouter([
-  {
-    path: "/messages",
-    element: <MessagePage />,
-  },
-  {
-    path: "/Profile",
-    element: <Profile />,
-  },
+    {
+        path: "/messages",
+        element: <MessagePage />,
+    },
+    {
+        path: "/Profile",
+        element: <Profile />,
+    },
     {
         path: "/request_management/received_offers",
         element: <ReceivedOffersPage />,
@@ -31,34 +31,34 @@ const router = createBrowserRouter([
         path: "/request_management/my_offers",
         element: <MyOffersPage />,
     },
-  {
-    path: "/myplants",
-    element: <MyPlants />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/",
-    element: <Index />
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/help_request_details/:requestId",
-    element: <HelpRequestDetailPage />,
-  },
-  {
-    path: "/create_request",
-    element: <CreateRequestPage />,
-  },
+    {
+        path: "/myplants",
+        element: <MyPlants />
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/add_plant",
+        element: <Index />
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+    {
+        path: "/",
+        element: <Homepage />,
+    },
+    {
+        path: "/help_request_details/:requestId",
+        element: <HelpRequestDetailPage />,
+    },
+    {
+        path: "/create_request",
+        element: <CreateRequestPage />,
+    },
 ])
 
 
