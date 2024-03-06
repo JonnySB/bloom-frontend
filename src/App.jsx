475159346e409c61b68fe3b1a7f5f119f89b1d58
Martynas Prusevicius <mprusevicius@gmail.com>
2024-03-06 13:47:01 +0000

    Fix Login to correct jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { MessagePage } from "./pages/MessagePage";
+import { MessagePage } from "./pages/Message/MessagePage";
 import { Login } from "./pages/Login/Login";
-import { Signup } from "./pages/Signup/Signup";
+import { Signup } from "./pages/Signup/SignupPage";
 import { MyPlants } from "./pages/MyPlants/MyPlants"
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./pages/MyPlants/MyPlants.css"
 import { Profile } from "./pages/Profile/ProfilePage";import { Index } from "./pages/Index/Index";
 import ReceivedOffersPage from "./pages/ManageHelpRequestPages/ReceivedOffersPage/ReceivedOffersPage";
 const router = createBrowserRouter([
