b17f16976de58975797edd32389a88af2ef690ce
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-13 12:33:34 +0000

    logic reworked

--- src/pages/Message/MessagePage.jsx
+++ src/pages/Message/MessagePage.jsx
@@ -6,12 +6,15 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { getUserInformationById } from "../../services/users";
 import NavbarComponent from "../../components/Navbar/NavbarComponent";
+import { getAllMessagesByUserId } from "../../services/messages";
 
 export const MessagePage = () => {
     const [selectedMessageId, setSelectedMessageId] = useState(null);
+    const [messages, setMessages] = useState([]);
     const [userDetails, setUserDetails] = useState(null);
     const [receiverDetails, setReceiverDetails] = useState(null);
     const [user_id, setuserID] = useState(window.localStorage.getItem("user_id"));
+    const [token, setToken] = useState(window.localStorage.getItem("token"));
 
     // help_offer_user_id passed from StartChatButton
     const location = useLocation();
@@ -22,52 +25,49 @@ export const MessagePage = () => {
         try {
           const userData = await getUserInformationById(user_id);
           setUserDetails(userData);
-
         } catch (err) {
           console.error('Error fetching user details:', err);
         }
       };
-    
-      fetchUserDetails();
-    }, [user_id]);
 
-    useEffect(() => {
-      const fetchReceiverDetails = async () => {
+      const fetchMessagesAndDetails = async () => {
         try {
-          if (help_offer_user_id !== undefined) {
-            const receiverMessageData = await getUserInformationById(help_offer_user_id);
-            setReceiverDetails(receiverMessageData);  
-          
-          } else if (help_offer_user_id == undefined) {
-            const receiverData = await getUserInformationById(3);
-            setReceiverDetails(receiverData);
+          const allMessages = await getAllMessagesByUserId(user_id, token);
+          setMessages(allMessages);
+
+          if (help_offer_user_id) {
+            const helpOfferDetails = await getUserInformationById(help_offer_user_id);
+            setReceiverDetails(helpOfferDetails);
           }
-        } catch(err) {
+        } catch (error) {
+          console.error("Error fetching messages or user details:", error);
         }
-      }
-      fetchReceiverDetails()
-    }, [help_offer_user_id, selectedMessageId?.sender_id])
+      };
 
+      fetchUserDetails();
+      fetchMessagesAndDetails();
+    }, [user_id, help_offer_user_id, token]);
 
-  //   const updateReceiverDetails = async (selectedMessage) => {
-  //     if (!selectedMessage) return;
-  //     try {
+    const handleChatSelect = async (selectedMessage) => {
+      console.log(selectedMessage)
+      // setSelectedMessageId(selectedMessage);
+      // const userId = selectedMessage.sender_id === user_id ? selectedMessage.recipient_id : selectedMessage.sender_id;
 
-  //         const receiverData = await getUserInformationById(selectedMessage.sender_id);
-  //         setReceiverDetails(receiverData);
-  //     } catch (err) {
-  //         console.error('Error fetching receiver details:', err);
-  //     }
-  // };
-  const handleChatSelect = (msg) => {
-        setSelectedMessageId(msg);
-    }; 
+      // if (!help_offer_user_id || userId !== help_offer_user_id) {
+      //   try {
+      //     const receiverData = await getUserInformationById(userId);
+      //     setReceiverDetails(receiverData);
+      //   } catch (err) {
+      //     console.error('Error fetching receiver details from selection:', err);
+      //   }
+      // }
+  };
  
     return (
       <>
         <NavbarComponent />
         <Container className="message-page-container">
-            <ChatListComponent onChatSelect={handleChatSelect} receiverDetails={receiverDetails} userDetails={userDetails} helpOferUserId={help_offer_user_id}/>
+            <ChatListComponent onChatSelect={handleChatSelect} messages_={messages} receiverDetails={receiverDetails} userDetails={userDetails} />
             {selectedMessageId && <MessageContainer messageManager={selectedMessageId} userDetails={userDetails}  receiverDetails={receiverDetails} />}
       </Container>
       </>
