269404552a13cc47b374d070909f1799c57ce322
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-17 21:05:06 +0000

    tests passing

--- src/components/MyPlants/ShowPlantsProfilePage.jsx
+++ src/components/MyPlants/ShowPlantsProfilePage.jsx
@@ -27,7 +27,7 @@ const PlantCards = ({myPlants, refreshPlants}) => {
       }
     }
   };
-  console.log(myPlants)
+
   return (
     <>
      <Container className="title">
@@ -73,30 +73,3 @@ const PlantCards = ({myPlants, refreshPlants}) => {
 
 export default PlantCards;
 
-
-
-// return (
-//   <>
-//   <Container className="title">
-//       <h1>Plants owned</h1>
-//       <Link to="/myplants" className="link-button">Expand</Link>
-//   </Container>
-//   <Row xs={1} md={5} className="plantcard">
-//     {userPlants?.slice(0, 5).reverse().map((plant, index) => (
-//       <Col key={index}>
-//         <Card>
-//           <Card.Body style={{ minHeight: "10rem" }}>
-//           <Card.Img variant="top" src={plant.photo} />
-//             <Card.Title>
-//               {plant.common_name} (<em>{plant.latin_name}</em>)
-//             </Card.Title>
-//             <Card.Text className="waterFrequency"> Watering Frequency: Approximately {plant.watering_frequency == 1 ? "once a week" : `${plant.watering_frequency} times a week`} </Card.Text>
-//             <Card.Text className="quantity"> Quantity: {plant.quantity} </Card.Text>
-//           </Card.Body>
-//         </Card>
-//       </Col>
-//     ))}
-//   </Row>
-//   </>
-// );
-// };
