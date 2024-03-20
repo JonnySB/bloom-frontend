6c57075e9f06eb034d5dfc83e74d5583dfb77d09
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-20 10:10:30 +0000

    styling added

--- src/components/Banner/HomePageBanner.jsx
+++ src/components/Banner/HomePageBanner.jsx
@@ -22,11 +22,8 @@ function SingleBannerComponent() {
     <div className='singleBannerComponent'>
     <CardLayout>
       {pictures.map((picture, index) => (
-        <CardTwo key={index}>
-           <Card.Title className="custom-card-title">{picture.label}</Card.Title>
-           <Card.Text className="custom-card-text">
-            {picture.description}
-            </Card.Text>
+        <CardTwo key={index} imageSrc={picture.src}>
+           <LabelRight>{picture.label}</LabelRight>
         </CardTwo>
       ))}
     </CardLayout>
@@ -34,24 +31,40 @@ function SingleBannerComponent() {
   );
 }
 
+
 const CardLayout = styled.div`
   margin: 0px 0px 1rem;
 `;
 
 
 const CardTwo = styled.div`
-  background: url(https://res.cloudinary.com/dououppib/image/upload/v1710928246/PLANTS/ff5uvfmzwconwahvivr9.avif) center center / cover;
+  background: ${({ imageSrc }) => `url(${imageSrc})`} center center / cover;
   height: 500px;
   border-radius: 7px;
+  position: relative; /* This makes it a positioning context for absolute children */
   display: flex;
-  flex-direction: column; /* This ensures children are stacked vertically */
+  flex-direction: column;
   align-items: center;
   text-align: center;
   justify-content: center;
-  padding: 20px; /* Adds some padding around the content */
+  padding: 20px;
 `;
+
+const LabelRight = styled.div`
+  position: absolute;
+  top: 0px; /* Adjust the value as needed */
+  right: 300px; /* Adjust the value as needed */
+  height: 110%;
+  width: 400px;
+  background-color: rgba(222, 190, 72, 0.5); /* Semi-transparent background */
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  font-weight: bold;
+  /* Add any other styles for your label here */
+`;
+
+
 
 export default SingleBannerComponent;
 
-
-//background-color: rgb(222 190 72 / var(--tw-bg-opacity));
